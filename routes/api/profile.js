506f5982938f3e9b797065a9f617f99a10ca3449
cameronmoodley <moodley.cameron@gmail.com>
2020-04-07 16:42:44 +0200

    added in route to update or create a profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,13 +5,14 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+
 // @route  GET api/profile/me
 // @desc   Get current users profile
 // @access Private
 router.get('/me', auth, async (req, res) => {
 	try {
 		const profile = await Profile.findOne({
-			user: req.user.id
+			user: req.user.id,
 		});
 
 		if (!profile) {
@@ -34,16 +35,14 @@ router.post(
 	[
 		auth,
 		[
-			check('status', 'Status is required').exists(),
-			check('skills', 'Skills is required').exists()
-		]
+			check('status', 'Status is required').not().isEmpty(),
+			check('skills', 'Skills is required').not().isEmpty(),
+		],
 	],
 	async (req, res) => {
 		const errors = validationResult(req);
-		console.log(errors);
-
 		if (!errors.isEmpty()) {
-			console.log('In here');
+			console.log('Code is throwing an issue');
 			return res.status(400).json({ errors: errors.array() });
 		}
 
@@ -59,7 +58,7 @@ router.post(
 			twitter,
 			instagram,
 			linkedin,
-			facebook
+			facebook,
 		} = req.body;
 
 		// Build profile object
@@ -73,14 +72,40 @@ router.post(
 		if (githubusername) profileFields.githubusername = githubusername;
 
 		if (skills) {
-			console.log('1234');
-			profileFields.skills = skills.split(',').map(skill => {
-				skill.trim();
+			profileFields.skills = skills.split(',').map((skill) => {
+				return skill.trim();
 			});
 		}
 
-		console.log(profileFields.skills);
-		return res.status(400).json({ errors: 'an error has occured' });
+		//Build Social Object
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (linkedin) profileFields.social.linkedin = linkedin;
+		if (instagram) profileFields.social.instagram = instagram;
+
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+			if (profile) {
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+
+				return res.json(profile);
+			}
+			// Create
+			profile = new Profile(profileFields);
+			await profile.save();
+			res.json(profile);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Server Error');
+		}
+
+		// return res.status(200).json({ success: 'No errors' });
 	}
 );
 
